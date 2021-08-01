b339397addaf013b6c127104def53d542caea60f
Erastus Mauliate <emauliate17@gmail.com>
2021-08-02 00:30:35 +0700

    Updated routes sport

--- routes/sport.js
+++ routes/sport.js
@@ -2,12 +2,12 @@ const SportRoutes = require("express").Router()
 const SportControllers = require("../controllers/SportControllers")
 
 SportRoutes.get("/", SportControllers.showSports)
+SportRoutes.get("/detail/:sportName", SportControllers.detailSport)
+SportRoutes.get("/delete/:id", SportControllers.removeSport)
 SportRoutes.get("/admin", SportControllers.showSportsByAdmin)
 SportRoutes.get('/admin/add', SportControllers.addSportPage)
 SportRoutes.post('/admin/add', SportControllers.addSport)
-SportRoutes.get("/detail/:sportName", SportControllers.detailSport)
 SportRoutes.get("/admin/edit/:sportName", SportControllers.editSportPage)
 SportRoutes.post("/admin/edit/:sportName", SportControllers.editSport)
-SportRoutes.get("/delete/:id", SportControllers.removeSport)
 
 module.exports = SportRoutes
